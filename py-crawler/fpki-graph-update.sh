1d73ef90ba61e565e3a9f3356b72a92ae45f0042
Robert Sherwood <robert.sherwood@credentive.com>
2023-08-04 10:10:06 +0000

    Rename variables

--- py-crawler/fpki-graph-update.sh
+++ py-crawler/fpki-graph-update.sh
@@ -10,11 +10,11 @@ SECRET_FILE="$SCRIPT_DIRECTORY/py_crawler/secrets/accesstoken"
 # Confirm env variable (if run from doocker) is present
 #------------------------------------------------------
 # Directory where we will install the Playbooks site from github
-if [ -v PLAYBOOKS_DIR ]; then
-  echo "Playbooks Directory set by docker to $PLAYBOOKS_DIR"
+if [ -v REPO_DIR ]; then
+  echo "Playbooks Directory set by docker to $REPO_DIR"
 else
-  echo "No \$PLAYBOOKS_DIR set. Setting to ../PLAYBOOKS_REPO"
-  PLAYBOOKS_DIR="../PLAYBOOKS_REPO"
+  echo "No \$REPO_DIR set. Setting to ../REPO"
+  REPO_DIR="../REPO"
 fi
 
 # Directory where we will keep the output of the command
@@ -49,21 +49,21 @@ fi
 # Set up GIT Variables
 #------------------------------------------------------
 # GIT REPO URL
-PLAYBOOKS_REPO="Credentive-Sec/ficam-playbooks"
+REPO="GSA/idmanagement.gov"
 # BRANCH FOR THIS RUN
 BRANCH=$(date +%m%d)-fpki-graph-update
 
-if ! test -d $PLAYBOOKS_DIR; then
+if ! test -d $REPO_DIR; then
     # If the playbooks site doesn't exist here, create it
-    echo "Setting \$PLAYBOOKS_DIR to $PLAYBOOKS_DIR"
-    mkdir $PLAYBOOKS_DIR
+    echo "Setting \$REPO_DIR to $REPO_DIR"
+    mkdir $REPO_DIR
 fi
 
 (
   echo "Executing gh auth login"
   echo $GH_TOKEN | gh auth login --with-token
   gh auth setup-git
-  cd "$PLAYBOOKS_DIR" || { echo "Playbooks directory does not exist!"; exit; }
+  cd "$REPO_DIR" || { echo "Playbooks directory does not exist!"; exit; }
 
   # See if we've already initialized the git repo here
   if [ "$(git rev-parse --is-inside-work-tree)" = "true" ]; then
@@ -73,10 +73,10 @@ fi
   else
     # initialize and update the repo
     echo "Initializing the playbooks REPO"
-    gh repo clone $PLAYBOOKS_REPO .
-    gh repo set-default $PLAYBOOKS_REPO
+    gh repo clone $REPO .
+    gh repo set-default $REPO
     git config user.name "py-crawler"
-    git config user.email "robert.sherwood@credentive.com"
+    git config user.email "robert.e.sherwood@gsa.gov"
   fi
 
   echo "Checking for local branch for the current run"
@@ -91,10 +91,10 @@ fi
 
   # Update the site with the new artifacts
   echo "Updating site with new artifacts"
-  cp "$OUTPUT_DIR/CACertificatesValidatingToFederalCommonPolicyG2.p7b" _fpki/tools/
-  cp "$OUTPUT_DIR/fpki-certs.gexf" _fpki/tools/
-  sed -e "s/\*\*Last Update\*\*: .*/\*\*Last Update\*\*: $(date +"%B %d, %Y")/" _fpki/tools/fpki_tools_graph.md > _fpki/tools/fpki_tools_graph.tmp
-  mv _fpki/tools/fpki_tools_graph.tmp _fpki/tools/fpki_tools_graph.md
+  cp "$OUTPUT_DIR/CACertificatesValidatingToFederalCommonPolicyG2.p7b" _implement/tools
+  cp "$OUTPUT_DIR/fpki-certs.gexf" _implement/tools
+  sed -e "s/\*\*Last Update\*\*: .*/\*\*Last Update\*\*: $(date +"%B %d, %Y")/" _implement/fpki_notifications.md > _implement/fpki_notifications.md.tmp
+  mv _implement/fpki_notifications.md.tmp _implement/fpki_notifications.md
 
   # Check for open PR
   # OPEN_PR=$(gh pr list --head $(date +%m%d)-fpki-graph-update --json number)
@@ -108,12 +108,12 @@ fi
 
   # Create Issue, record the output to a variable
   echo "Creating Issue"
-  ISSUE=$(gh issue create --repo "$PLAYBOOKS_REPO" --title "$(date +%m%d) FPKI Graph Update"  --body "Automatically Created")
+  ISSUE=$(gh issue create --repo "$REPO" --title "$(date +%m%d) FPKI Graph Update"  --body "Automatically Created")
 
   # Parse out the issue number
   ISSUE_NUM=$(echo $ISSUE | cut -f 7 -d "/")
 
   # Open a PR linked to the Issue
   echo "Creating PR"
-  gh pr create --repo "$PLAYBOOKS_REPO" --head "$BRANCH" --base "staging" --title "$(date +%m%d) Fpki Graph Update" --body "Closes #$ISSUE_NUM"
+  gh pr create --repo "$REPO" --head "$BRANCH" --base "staging" --title "$(date +%m%d) Fpki Graph Update" --body "Closes #$ISSUE_NUM"
 )
