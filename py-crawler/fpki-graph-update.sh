f66a06bd7f62cbd6cad45beb334e27989e8ed630
Rob Sherwood <robert.sherwood@credentive.com>
2024-04-11 15:21:53 -0400

    More updates to shell script

--- py-crawler/fpki-graph-update.sh
+++ py-crawler/fpki-graph-update.sh
@@ -4,8 +4,9 @@
 # SCRIPT DIRECTORY
 SCRIPT_DIRECTORY=$(pwd)
 # Secret file location
-SECRET_FILE="$SCRIPT_DIRECTORY/py_crawler/secrets/accesstoken"
-SIGNING_KEY_PUB="$SCRIPT_DIRECTORY/py_crawler/secrets/signing_key.pub"_PUB
+ACCESS_TOKEN_FILE="$SCRIPT_DIRECTORY/py_crawler/secrets/accesstoken"
+SIGNING_KEY_PUB="$SCRIPT_DIRECTORY/py_crawler/secrets/signing_key.pub"
+SIGNING_KEY="$SCRIPT_DIRECTORY/py_crawler/secrets/signing_key"
 
 #------------------------------------------------------
 # Confirm env variable (if run from doocker) is present
@@ -18,6 +19,12 @@ else
   REPO_DIR="../REPO"
 fi
 
+if ! test -d $REPO_DIR; then
+    # If the playbooks site doesn't exist here, create it
+    echo "Creating $REPO_DIR"
+    mkdir $REPO_DIR
+fi
+
 # Directory where we will keep the output of the command
 if [ -v OUTPUT_DIR ]; then
   echo "Output Directory set by docker to $OUTPUT_DIR"
@@ -26,24 +33,32 @@ else
   OUTPUT_DIR="../OUTPUT"
 fi
 
-
-#------------------------------------------------------
-# Run py_crawler
-#------------------------------------------------------
-poetry run python -m py_crawler
-
-#------------------------------------------------------
-# Update Playbooks site with new artifacts
-#------------------------------------------------------
-
 #------------------------------------------------------
 # Confirm secret is present
 #------------------------------------------------------
-if test -f "$SECRET_FILE"; then
+if test -f "$ACCESS_TOKEN_FILE"; then
   # Set Access Token
-  GH_TOKEN=$(cat "$SECRET_FILE")
+  GH_TOKEN=$(cat "$ACCESS_TOKEN_FILE")
 else
-  echo No secret file present at $SECRET_FILE
+  echo No secret file present at $ACCESS_TOKEN_FILE
+  exit 1
+fi
+
+#------------------------------------------------------
+# Configuring signing keys 
+# (this should only need to be done once, but is low impact, so we'll just do it every time)
+#------------------------------------------------------
+if test -f "$SIGNING_KEY_PUB" -a -f "$SIGNING_KEY"; then
+  echo "Starting ssh-agent"
+  eval $(ssh-agent) > /dev/null
+
+  echo "Adding private signing key to ssh-agent"
+  # Set permissions properly
+  chmod 0600 $SIGNING_KEY
+  ssh-add $SIGNING_KEY
+else
+  echo Signing keys not found at $SIGNING_KEY or $SIGNING_KEY_PUB
+  exit 1
 fi
 
 #------------------------------------------------------
@@ -54,11 +69,16 @@ REPO="GSA/idmanagement.gov"
 # BRANCH FOR THIS RUN
 BRANCH=$(date +%m%d)-fpki-graph-update
 
-if ! test -d $REPO_DIR; then
-    # If the playbooks site doesn't exist here, create it
-    echo "Setting \$REPO_DIR to $REPO_DIR"
-    mkdir $REPO_DIR
-fi
+
+#------------------------------------------------------
+# Run py_crawler
+#------------------------------------------------------
+echo "Running py_crawler. This will take a minute..."
+poetry run python -m py_crawler
+
+#------------------------------------------------------
+# Update Playbooks site with new artifacts
+#------------------------------------------------------
 
 (
   echo "Executing gh auth login"
@@ -70,12 +90,14 @@ fi
   if [ "$(git rev-parse --is-inside-work-tree)" = "true" ]; then
     # switch to staging branch
     echo "Switching to 'staging' branch"
-    git branch staging
+    git switch -f staging
     # sync the repo
     echo "Repo found. Syncing..."
     gh repo sync --force
     echo "Deleting branches that have been merged"
     git branch --merged | xargs git branch -d
+    echo "Deleting local references to branches that have been deleted on remote"
+    git fetch --prune
   else
     # initialize and update the repo
     echo "Initializing the playbooks REPO"
@@ -83,6 +105,9 @@ fi
     gh repo set-default $REPO
     git config user.name "py-crawler"
     git config user.email "robert.e.sherwood@gsa.gov"
+    echo "Configuring Git with public signing key"
+    git config gpg.format ssh
+    git config user.signingkey $SIGNING_KEY_PUB
   fi
 
   echo "Checking for local branch for the current run"
@@ -106,14 +131,10 @@ fi
   # Check for open PR
   # OPEN_PR=$(gh pr list --head $(date +%m%d)-fpki-graph-update --json number)
 
-  # Configuring signing keys (this should only need to be done once, but is low impact, so we'll just do it every time)
-  echo "Configuring Signing Key"
-  git config gpg.format ssh
-  git config user.signingkey $SIGNING_KEY_PUB
-
   # Submit the playbooks updates to the git repo
-  echo "Adding and commiting updates"
+  echo "Adding updates"
   git add -A
+  echo "Creating Signed Commit"
   git commit -S -m "automatic crawler update"
   echo "Submitting updates to origin"
   git push --all
